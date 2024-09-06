7afc7ba14721aee3bc2f1093ed4c596e75eff905
Mazo667 <maxifava667@gmail.com>
2024-09-06 06:13:13 -0300

    Creo el modelo mensaje y hago las migraciones correspondientes

--- TableroMensajes/mensajes/models.py
+++ TableroMensajes/mensajes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Mensaje(models.Model):
+    remitente = models.CharField(max_length=255)
+    destinatario = models.CharField(max_length=255)
+    texto = models.TextField()
+    fecha_hora = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.remitente} a {self.destinatario}: {self.texto}"
