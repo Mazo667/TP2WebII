df9d4aff2a838b5a42e2126108205de69678a9cb
Mazo667 <maxifava667@gmail.com>
2024-09-07 00:01:43 -0300

    Creacion de vistas y sus respectivas plantillas

--- TableroMensajes/mensajes/urls.py
+++ TableroMensajes/mensajes/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
+from .views import MensajeCreateView, MensajeDeleteView, MensajeListView, index, MensajeCreated
 
 app_name = 'mensajes'
 
+urlpatterns = [
+    path('',index,name='index'),
+    path('mensajes/', MensajeListView.as_view(), name='mensaje_list'),
+    path('mensaje/crear',MensajeCreateView.as_view(),name='mensaje_create'),
+    path('mensaje/creado/', MensajeCreated, name='mensaje_created'),
+    path('mensajes/delete/<int:pk>/', MensajeDeleteView.as_view(), name='mensaje_delete')
+]
--- TableroMensajes/mensajes/views.py
+++ TableroMensajes/mensajes/views.py
@@ -3,22 +3,31 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Mensaje
 from .forms import MensajeForm
 
-class MensajeView(View):
+def index(request):
+    return render(request,'mensajes/index.html')
+
+def MensajeCreated(request):
+    return render(request,'mensajes/mensaje_created.html')
+
+class MensajeListView(View):
     def get(self, request):
         mensajes = Mensaje.objects.all()
+        return render(request, 'mensajes/mensaje_list.html', {'mensajes': mensajes})
+
+class MensajeCreateView(View):
+    def get(self, request):
         form = MensajeForm()
-        return render(request, 'mensajes/mensaje_list.html', {'mensajes': mensajes, 'form': form})
+        return render(request, 'mensajes/mensaje_create.html', {'form': form})
 
     def post(self, request):
         form = MensajeForm(request.POST)
         if form.is_valid():
-            form.save() 
-            return redirect('mensaje_list') 
-        mensajes = Mensaje.objects.all() 
-        return render(request, 'mensajes/mensaje_list.html', {'mensajes': mensajes, 'form': form})
+            form.save()
+            return redirect('mensajes:mensaje_created')  
+        return render(request, 'mensajes/index.html', {'form': form})
 
 class MensajeDeleteView(View):
     def post(self, request, pk):
         mensaje = get_object_or_404(Mensaje, pk=pk)
         mensaje.delete() 
-        return redirect('mensaje_list') 
+        return redirect('mensajes:mensaje_list') 
