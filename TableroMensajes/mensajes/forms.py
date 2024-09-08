72cb2376e673aec1f79cdc7a82fe5a8a4470bb2d
Mazo667 <maxifava667@gmail.com>
2024-09-07 21:59:22 -0300

    Mejora en el formulario

--- TableroMensajes/mensajes/forms.py
+++ TableroMensajes/mensajes/forms.py
@@ -4,4 +4,14 @@ from .models import Mensaje
 class MensajeForm(forms.ModelForm):
     class Meta:
         model = Mensaje
-        fields = ['remitente','destinatario','texto']
+        fields = ['remitente', 'destinatario', 'texto']
+        widgets = {
+            'remitente': forms.TextInput(attrs={'class': 'form-control'}),
+            'destinatario': forms.TextInput(attrs={'class': 'form-control'}),
+            'texto': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+        labels = {
+            'remitente': 'Remitente',
+            'destinatario': 'Destinatario',
+            'texto': 'Mensaje',
+        }
